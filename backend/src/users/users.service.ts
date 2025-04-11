9c085432a1554f38e167b8f377bb9fff5d6e6bf6
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-11 11:39:43 +0200

    Inject User repository for orm

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -4,4 +4,8 @@ import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class UsersService {}
+  constructor(
+    @InjectRepository(User)
+    private readonly usersRepository: Repository<User>,
+  ) {}
+
