1a54a8984b28662dc1844a60fe2451d0f05e400d
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-11 16:58:05 +0200

    Create usestates for password, email, isLoggidIn, errorMessage and isLoading

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react';
+import logo from '/public/logo.png';
+import { API_BASE_URL } from '../constants';
+import OpenEyeIcon from './OpenEyeIcon';
+import ClosedEyeIcon from './ClosedEyeIcon';
+import LoadingSpinner from './LoadingSpinner';
+import LoginFeedbackMessage from './LoginFeedbackMessage';
 
 function LoginForm() {
+  const [showPassword, setShowPasssword] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
     <div>LoginForm</div>
   )
