f4af3371afa850e417cc91373e6ecaad106110b0
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-11 17:00:28 +0200

    Implement App by adding LoginPage & bg decor & base style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import LoginPage from './pages/LoginPage';
+import bgImage from '/public/bg-image.svg';
 
 function App() {
   return (
-    <div>
+    <div className="bg-gradient-to-r to-30% from-blue-900 to-blue-950 h-screen flex items-center justify-center">
+      <img src={bgImage} alt="Background decoration" className="absolute right-0 bottom-0 h-3/4 object-cover" />
       <LoginPage />
     </div>
   );
