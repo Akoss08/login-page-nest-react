49e72efd89fc2f4a21aa91e6d7f8641288866fc4
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-11 11:40:10 +0200

    Implement findUserByEmail method

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -4,8 +4,13 @@ import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
+export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
   ) {}
 
+  async findUserByEmail(email: string): Promise<User | null> {
+    return await this.usersRepository.findOne({ where: { email } });
+  }
+}
