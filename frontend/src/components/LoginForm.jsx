401841b99ea6869ef20d3bc5b400cefd46cfbf5c
Akos Horvath <156071555+Akoss08@users.noreply.github.com>
2025-04-11 16:58:28 +0200

    Implement handleSubmit method for login

--- frontend/src/components/LoginForm.jsx
+++ frontend/src/components/LoginForm.jsx
@@ -14,6 +14,46 @@ function LoginForm() {
   const [errorMessage, setErrorMessage] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
+  async function handleSubmit(event) {
+    try {
+      event.preventDefault();
+      setIsLoading(true);
+
+      const credentials = {
+        email,
+        password,
+      };
+
+      const loginRes = await fetch(`${API_BASE_URL}/auth/login`, {
+        method: 'Post',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credentials),
+      });
+
+      if (!loginRes.ok) {
+        const errorData = await loginRes.json();
+        setErrorMessage(errorData.message);
+        return;
+      }
+
+      const loginData = await loginRes.json();
+      console.log('Login API response:', loginData);
+
+      setIsLoggedIn(true);
+    } catch (error) {
+      alert('Valami hiba történt, próbálja újra!');
+
+      console.error(error);
+      setIsLoggedIn(false);
+    } finally {
+      setEmail('');
+      setPassword('');
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div>LoginForm</div>
   )
